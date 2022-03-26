177daa8644aaf30c1b8e1fbc49aa22a6fbb5751f
SakoPak <sako.taya@gmail.com>
2022-03-25 21:36:01 -0700

    Add edits to app.js files
    Added event listeners and required events.js

--- app/app.js
+++ app/app.js
@@ -1,9 +1,13 @@
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
+const authEvents = require('./auth/events.js')
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
-  // your JS code goes here
+  $('#signUp').on('submit', authEvents.onSignUp)
+  $('#signIn').on('submit', authEvents.onSignIn)
+  $('#signOut').on('click', authEvents.onSignOut)
+  $('#createGame').on('click', authEvents.onCreateGame)
 })
