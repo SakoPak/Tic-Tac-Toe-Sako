12573b7e1410a8dd76ee06902cefc601574b51bd
SakoPak <sako.taya@gmail.com>
2022-03-28 22:18:20 -0700

    Edit UI file
    Add different display messages, storing player and game tokens

--- app/auth/ui.js
+++ app/auth/ui.js
@@ -3,21 +3,23 @@
 const store = require('../store.js')
 
 const onSignUpSuccess = function () {
-  $('#auth-display').html('<p>Thanks for signing up!</p>')
+  $('#sign-up-success').html('<p>Thanks for signing up!</p>')
   $('form').trigger('reset')
 }
 
 const onSignUpFailure = function (response) {
-  $('#auth-display').html('<p>Error, please try again.</p>')
+  $('#sign-up-success').html('<p>Error, please try again.</p>')
   $('form').trigger('reset')
 }
 
 const onSignInSuccess = function (response) {
-  $('#auth-display').html('<p>Get ready to play Tic Tac Toe!</p>')
+  $('#sign-in-success').html('<p>Get ready to play Tic Tac Toe!</p>')
   $('form').trigger('reset')
+  store.user = response.user
 }
+
 const onSignInFailure = function () {
-  $('#auth-display').html('Could not verify player, please sign in again!')
+  $('#auth-display').html('<p>Could not verify player, please sign in again!</p>')
   $('form').trigger('reset')
 }
 
@@ -31,9 +33,10 @@ const onSignOutFailure = function () {
   $('#auth-display').html('<p>Error while signing out, please try again.</p>')
 }
 
-const onCreateGameSuccess = function () {
+const onCreateGameSuccess = function (response) {
   $('#auth-display').html('<p>Begin playing! You are X.</p>')
-  $('form').trigger('reset')
+  console.log(response)
+  store.game = response.game
 }
 
 const onCreateGameFailure = function () {
@@ -41,9 +44,12 @@ const onCreateGameFailure = function () {
   $('form').trigger('reset')
 }
 
-const onUpdateGameSuccess = function () {
-  // $('#auth-display').html('<p>Error, please try again.</p>')
-  $('form').trigger('reset')
+// retrieving data every time user makes a new move
+const gameArray = ['', '', '', '', '', '', '', '', '']
+const onUpdateGame = function (index, value, gameStanding) {
+  gameArray[index] = index
+  store.currentPlayer = value
+  store.gameStanding = gameStanding
 }
 
 const onUpdateGameFailure = function () {
@@ -65,7 +71,7 @@ module.exports = {
   onSignOutFailure,
   onCreateGameSuccess,
   onCreateGameFailure,
-  onUpdateGameSuccess,
+  onUpdateGame,
   onUpdateGameFailure,
   onPlayAgain
 }
